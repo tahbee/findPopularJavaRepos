75db81377e0d4640729411e45d39db6c9a111c69
Thanjila Uddin <thanjila.u@gmail.com>
2021-10-12 22:15:37 -0400

    Update script to find trending repos api

--- main.py
+++ main.py
@@ -3,57 +3,48 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
+repo_number = 0
 
-# def url_generator():
-#     with open('urls.txt', 'r') as reader:
-#         return reader.readlines()
-# def extract_elements_to_csv():
-#         page = requests.get(url.strip('\n\r'))
-#         soup = BeautifulSoup(page.content, 'html.parser')
-#         elements = soup.find_all('a', class_="social-count")
-#         starred = elements[0].getText()
-#         if 'k' in starred:
-#             starred = float(starred.split('k')[0])*1000
-#         forked = elements[1].getText()
-#         if 'k' in forked:
-#             forked = float(forked.split('k')[0]) * 1000
-#         commit_elements = soup.find_all('span', class_="d-none d-sm-inline")
-#         commits = commit_elements[1].getText().split()[0]
-#         license_element = soup.find('a', class_="Link--muted")
-#         writer.writerow([url.strip(), int(starred), int(forked), int(commits.strip().replace(",", "")), license_element.getText().strip()])
-# def web_scraping():
-#     global writer, url
-#     githubUrls = url_generator()
-#     with open('final_results.csv', mode='w') as result_file:
-#         writer = csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#         writer.writerow(["url", "starred", "forked", "commits", "license"])
-#         for url in githubUrls:
-#             extract_elements_to_csv()
+def access_repo(url):
+    response = requests.get(url)
+    print(response.status_code)
+    repos = response.json()
+    for repo in repos:
+        repo_number = repo_number + 1
+        print("Accessing repo number")
+        print(repo_number)
+        get_repo_info(repo)
 
-
-def get_repo_info():
-    page = requests.get(repo['html_url'])
+def get_repo_info(repo):
+    page = requests.get(repo['url'])
     soup = BeautifulSoup(page.content, 'html.parser')
     try:
         commit_elements = soup.find_all('span', class_="d-none d-sm-inline")
-        commits = commit_elements[1].getText().split()[0]
+        commits = commit_elements[1].getText().split()[0].replace(',', '')
     except IndexError:
         commits= "0" # if coud not find commits set as 0 for now- have to manually find them
 
-    license = "None"
-    if repo['license']:
-        license = repo['license']['name']
-    writer.writerow(
-        [repo['name'], repo['html_url'], repo['stargazers_count'], repo['forks_count'], int(commits.replace(",","")), license, repo['updated_at'], repo['size'], repo['visibility'] ])
-
+    if (int(commits) > 500):
+        writer.writerow(
+            [
+                repo['repositoryName'], 
+                repo['url'],
+                commits,
+                repo['totalStars'],
+                repo['forks'],
+                repo['rank'],
+                repo['since'],
+                repo['username']
+                ]
+        )
 
 if __name__ == '__main__':
-    with open('results2.csv', mode='w') as result_file:
+    with open('collected_repos.csv', mode='w') as result_file:
         writer = csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["name", "url", "starred", "forked", "commits", "license", "last-updated", "size", "visibility"])
-        for page in range(1, 4):
-            response = requests.get("https://api.github.com/search/repositories?q=language:Java&sort=stars&order=desc&per_page=100&page="+str(page))
-            print(response.status_code)
-            repos = response.json()['items']
-            for repo in repos:
-                get_repo_info()
+        writer.writerow(["repositoryName", "url", "commits", "totalStars", "forks", "rank", "since", "username"])
+        url1 = "https://gh-trending-api.herokuapp.com/repositories/java?since=monthly&spoken_language_code=en"
+        url2 = "https://gh-trending-api.herokuapp.com/repositories/java?since=weekly&spoken_language_code=en"
+        url3 = "https://gh-trending-api.herokuapp.com/repositories/java?since=daily&spoken_language_code=en"
+        access_repo(url1)
+        access_repo(url2)
+        access_repo(url3)
